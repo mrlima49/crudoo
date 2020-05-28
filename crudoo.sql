dd66fc5e575973cef02c25bbe5f6f9c003eb9729
mrlima49 <mrlima49@yahoo.coom>
2020-05-28 20:54:45 -0300

    atualização do index e inserção do dump do banco de dados

---
+++ crudoo.sql
@@ -0,0 +1,43 @@
+-- --------------------------------------------------------
+-- Servidor:                     localhost
+-- Versão do servidor:           5.7.24 - MySQL Community Server (GPL)
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Copiando estrutura do banco de dados para crudpoo
+DROP DATABASE IF EXISTS `crudpoo`;
+CREATE DATABASE IF NOT EXISTS `crudpoo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `crudpoo`;
+
+-- Copiando estrutura para tabela crudpoo.contatos
+CREATE TABLE IF NOT EXISTS `contatos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela crudpoo.contatos: ~6 rows (aproximadamente)
+/*!40000 ALTER TABLE `contatos` DISABLE KEYS */;
+INSERT INTO `contatos` (`id`, `nome`, `email`) VALUES
+	(1, 'alexandre lima', 'alexandre@gmail.com'),
+	(2, 'hazel moore', 'hazel@gmail.com'),
+	(4, 'maria lima', 'maria@gmail.com'),
+	(5, 'indica', 'indica@gmail.com'),
+	(6, 'santos', 'santos@gmail.com'),
+	(7, 'james', 'james@gmail.com'),
+	(9, 'paula', 'paula@gmail.com');
+/*!40000 ALTER TABLE `contatos` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 
 require_once "./Contato.php";
 
+echo "<h1>CrudOO</h1>";
+echo "<hr>";
 $contato = new Contato();
 $contato->read();
